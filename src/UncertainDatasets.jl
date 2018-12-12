284717defc80498e6006490fe2b79183ec9716b7
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-12-13 00:41:17 +0100

    Don't include uncertain statistics yet.

--- src/UncertainDatasets.jl
+++ src/UncertainDatasets.jl
@@ -12,7 +12,4 @@ include("uncertain_values/uncertain_values.jl")
 include("uncertain_datasets/uncertain_datasets.jl")
 include("sampling/sampling.jl")
 
-# Dispatch for functions from other packages
-include("uncertain_statistics/UncertainStatistics.jl")
-
 end # module
