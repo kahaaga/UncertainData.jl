d18811f804e3440137f162b0ad73ddcfd9831014
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-12-13 11:16:51 +0100

    Update docs for UncertainValue

--- src/uncertain_values/UncertainValue.jl
+++ src/uncertain_values/UncertainValue.jl
@@ -5,9 +5,9 @@ include("UncertainScalars.jl")
 """
     UncertainValue(empiricaldata::AbstractVector{T}, distribution) where {T <: Number}
 
-## Constructor for empirical distributions.
+# Constructor for empirical distributions
 
-### Arguments
+## Arguments
 - **`empiricaldata`**: The data for which to fit the `distribution`.
 - **`distribution`**: A valid univariate distribution from `Distributions.jl`.
 
@@ -21,31 +21,20 @@ end
 
      UncertainValue(a, b, distribution; kwargs...)
 
-## Constructor for two-parameter distributions
+# Constructor for two-parameter distributions
 
 `UncertainValue`s are currently implemented for the following two-parameter
 distributions: `Uniform`, `Normal`, `Binomial`, `Beta`, `BetaPrime`, `Gamma`,
 and `Frechet`.
 
-Examples:
-- **`UncertainValue(μ, σ, Normal; kwargs...)`**. A normal distribution with mean
-    `μ` and standard deviation `σ/nσ` (`nσ` must be given as a keyword argument).
-    - Example 1: `UncertainValue(2.3, 0.3, Normal)`. A normal distribution with
-        `mean = 2.3` and standard deviation `0.3`.
-    - Example 2: `UncertainValue(2.3, 0.3, Normal, nσ = 2)`. A normal distribution
-        with `mean = 2.3` and `standard deviation = 0.3/2`.
-    - Example 3: `UncertainValue(2.3, 0.3, Normal, trunc_lower = 1.0, trunc_upper = 3.0)`.
-        A normal distribution with `mean = 2.3` and `standard deviation = 0.3`, truncated to the interval `[1, 3]`.
-- **`UncertainValue(lower, upper, Uniform)`**. A uniform distribution on
-    `[lower, upper]`.
-    - Example 1: `UncertainValue(-2, 3, Uniform)`. A uniform distribution on `[2, 3]`.
-
 ### Arguments
+
 - **`a`, `b`**: Generic parameters whose meaning varies depending
     on what `distribution` is provided. See the list above.
 - **`distribution`**: A valid univariate distribution from `Distributions.jl`.
 
 ### Keyword arguments
+
 - **`nσ`**: If `distribution <: Distributions.Normal`, then how many standard
     deviations away from `μ` does `lower` and `upper` (i.e. both, because
     they are the same distance away from `μ`) represent?
@@ -56,6 +45,37 @@ Examples:
     support. Defaults to `-Inf`.
 - **`trunc_upper`**: Upper truncation bound for distributions with infinite
     support. Defaults to `Inf`.
+
+## Examples
+
+### Normal distribution
+
+Normal distributions are formed by using the constructor
+`UncertainValue(μ, σ, Normal; kwargs...)`. This gives a normal distribution with
+mean μ and standard deviation σ/nσ (nσ must be given as a keyword argument).
+
+```julia
+# A normal distribution with mean = 2.3 and standard deviation 0.3.
+UncertainValue(2.3, 0.3, Normal)
+
+# A normal distribution with mean 2.3 and standard deviation 0.3/2.
+UncertainValue(2.3, 0.3, Normal, nσ = 2)
+
+# A normal distribution with mean 2.3 and standard deviation = 0.3,
+truncated to the interval `[1, 3]`.
+UncertainValue(2.3, 0.3, Normal, trunc_lower = 1.0, trunc_upper = 3.0)
+```
+
+#### Uniform distribution
+
+Uniform distributions are formed using the
+`UncertainValue(lower, upper, Uniform)` constructor.
+
+```julia
+#  A uniform distribution on `[2, 3]`
+UncertainValue(-2, 3, Uniform)
+```
+
 """
 function UncertainValue(a::T1, b::T2, distribution;
         kwargs...) where {T1<:Number, T2 <: Number}
@@ -91,12 +111,7 @@ end
 
 ## Constructor for three-parameter distributions
 
-Currently implemented:
-
-- **`UncertainValue(μ, lower, upper, distribution; kwargs...)`**. A normal
-    distribution with mean `μ` with standard deviation `(μ - lower)/nσ` (or,
-    equivalently, `(upper - μ)/nσ`). Here, `lower` and `upper` must be symmetric
-    around `μ`. It is better to use the two-parameter constructor for this case.
+Currently implemented distributions are `BetaBinomial`.
 
 ### Arguments
 - **`a`, `b`, `c`**: Generic parameters whose meaning varies depending
