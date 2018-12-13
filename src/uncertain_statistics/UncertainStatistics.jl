9bc5d0ca64960393d7de45a0223c0a3c449aa1de
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-12-13 03:26:05 +0100

    Gather uncertain statistics

--- src/uncertain_statistics/UncertainStatistics.jl
+++ src/uncertain_statistics/UncertainStatistics.jl
@@ -1,5 +1,11 @@
+using StatsBase
+
 
 # Definitions of statistical methods for UncertainDatasets
 include("core_stats.jl")
-include("StatsBase_meanfunctions.jl")
-include("StatsBase_scalarstatistics_moments.jl")
+#include("StatsBase_meanfunctions.jl")
+#include("StatsBase_scalarstatistics_moments.jl")
+#include("StatsBase_variation.jl")
+#include("StatsBase_zscores.jl")
+#include("StatsBase_entropies.jl")
+#include("StatsBase_quantiles.jl")
