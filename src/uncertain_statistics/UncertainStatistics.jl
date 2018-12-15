e915fe0165c714f06b7e6e92708116fcf5b7cabe
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-12-15 16:29:51 +0100

    Refactor hypothesis tests, including original copyright notices

--- src/uncertain_statistics/UncertainStatistics.jl
+++ src/uncertain_statistics/UncertainStatistics.jl
@@ -1,14 +1,22 @@
-using StatsBase
+using Reexport
 
+@reexport module UncertainStatistics
+    using StatsBase
+    using Distributions
+    using ..UncertainValues
+    using ..UncertainDatasets
 
-# Definitions of statistical methods for UncertainDatasets
-include("core_stats.jl")
-include("HypothesisTests.jl")
-include("HypothesisTests_timeseries.jl")
+    # Definitions of statistical methods for UncertainDatasets
+    include("statsbase/core_stats.jl")
+    include("hypothesis_tests/mann_whitney.jl")
+    include("hypothesis_tests/t_tests.jl")
+    include("hypothesis_tests/HypothesisTests.jl")
+    include("hypothesis_tests/HypothesisTests_timeseries.jl")
 
-#include("StatsBase_meanfunctions.jl")
-#include("StatsBase_scalarstatistics_moments.jl")
-#include("StatsBase_variation.jl")
-#include("StatsBase_zscores.jl")
-#include("StatsBase_entropies.jl")
-#include("StatsBase_quantiles.jl")
+    #include("StatsBase_meanfunctions.jl")
+    #include("StatsBase_scalarstatistics_moments.jl")
+    #include("StatsBase_variation.jl")
+    #include("StatsBase_zscores.jl")
+    #include("StatsBase_entropies.jl")
+    #include("StatsBase_quantiles.jl")
+end
