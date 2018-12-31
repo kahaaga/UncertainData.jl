067f445dcec2f8e55e123a43d306e0214876ad4d
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2018-12-31 02:50:56 +0100

    Resample datasets with constraints (#23)
    * Resampling stuff, updated tests
    
    * Updated docs and README

--- test/resampling/test_resampling_datasets.jl
+++ test/resampling/test_resampling_datasets.jl
@@ -1,4 +1,14 @@
 import KernelDensity.UnivariateKDE
+o1 = UncertainValue(Normal, 0, 0.5)
+o2 = UncertainValue(Normal, 2.0, 0.1)
+o3 = UncertainValue(Uniform, 0, 4)
+o4 = UncertainValue(Uniform, rand(100))
+o5 = UncertainValue(Beta, 4, 5)
+o6 = UncertainValue(Gamma, 4, 5)
+o7 = UncertainValue(Frechet, 1, 2)
+o8 = UncertainValue(BetaPrime, 1, 2)
+o9 = UncertainValue(BetaBinomial, 10, 3, 2)
+o10 = UncertainValue(Binomial, 10, 0.3)
 
 D = UncertainDataset([o1, o2, o3, o4, o5, o6, o7, o8, o9, o10])
 UV = UncertainValueDataset(D)
