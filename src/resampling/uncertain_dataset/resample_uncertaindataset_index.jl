1d6b18425f865c545af35e403d22337f5f54c828
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-08-19 01:12:59 +0200

    Fix doc string

--- src/resampling/uncertain_dataset/resample_uncertaindataset_index.jl
+++ src/resampling/uncertain_dataset/resample_uncertaindataset_index.jl
@@ -12,7 +12,7 @@ function resample(uvd::UncertainIndexDataset)
 end
 
 """
-	resample(uvd::UncertainIndexDataset)
+	resample(uvd::UncertainIndexDataset, n::Int)
 
 Draw `n` realisations of an `UncertainIndexDataset` according to the
 distributions of the `UncertainValue`s comprising it.
