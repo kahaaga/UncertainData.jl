b01f396a9f25493d2df50fb304f9280b58633ff1
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-10-08 03:33:22 +0200

    Some documentation changes

--- src/sampling_constraints/constraint_definitions.jl
+++ src/sampling_constraints/constraint_definitions.jl
@@ -70,6 +70,13 @@ end
 
 A constraint indicating that the distribution furnishing an uncertain value
 should be truncated at the mean ± `nσ` (`n` standard deviations).
+
+## Notes 
+
+- Beware when you apply the `TruncateStd` constraint to a (usually a numeric)
+    population with a small value range. With `nσ` small, you might end up with 
+    a population mean *between* the actual values, so that the range 
+    `[mean(pop) - nσ*std(pop), mean(pop) + nσ*std(pop)]` returns `nothing`.
 """
 struct TruncateStd{T<:Number} <: ValueSamplingConstraint
     nσ::T
