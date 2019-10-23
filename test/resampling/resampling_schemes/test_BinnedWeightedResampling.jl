1949548d5e23a667d90a375a1266fe28b6756037
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-10-24 01:17:22 +0200

    Update test

--- test/resampling/resampling_schemes/test_BinnedWeightedResampling.jl
+++ test/resampling/resampling_schemes/test_BinnedWeightedResampling.jl
@@ -3,4 +3,4 @@ grid = 0:10:500
 wts = Weights(rand(10))
 resampling = BinnedWeightedResampling(grid, wts, n_draws)
 
-@test resampling isa BinnedResampling
+@test resampling isa BinnedWeightedResampling
