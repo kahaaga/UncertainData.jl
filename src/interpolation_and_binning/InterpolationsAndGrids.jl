90d4b8bf2a08d21862ccc865c0720a3c6759190b
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-11-04 15:39:56 +0100

    Change folder structure

--- src/interpolation_and_binning/InterpolationsAndGrids.jl
+++ src/interpolation_and_binning/InterpolationsAndGrids.jl
@@ -4,6 +4,10 @@ using Reexport
     using Interpolations
     
     include("findall_nan_chunks.jl")
+    include("fill_nans.jl")
+
     include("grids/RectangularGrid.jl")
     include("methods/interpolation.jl")
+    include("interpolate_and_bin.jl")
+    include("binning.jl")
 end
