df9135f82fe4eae8b7d94bd6b7943a008c0833c1
Kristian Agasøster Haaga <kahaaga@gmail.com>
2021-02-16 15:47:47 +0100

    Update CI

--- docs/make.jl
+++ docs/make.jl
@@ -134,13 +134,3 @@ if CI
         push_preview = true
     )
 end
-
-# if !Sys.iswindows()
-#     deploydocs(
-#         deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4",
-#         "python-markdown-math", "pygments", "pymdown-extensions"),
-#         repo   = "github.com/kahaaga/UncertainData.jl.git",
-#         target = "site",
-#         make = () -> run(`mkdocs build`)
-#     )
-# end
