b3fae906b0979ab0bcfcfc5512773fb8d6b9182a
Kristian Agasøster Haaga <kahaaga@gmail.com>
2021-04-21 16:13:06 +0200

    Update tests

--- test/resampling/uncertain_datasets/sequential/test_resampling_sequential_increasing.jl
+++ test/resampling/uncertain_datasets/sequential/test_resampling_sequential_increasing.jl
@@ -1,90 +1,65 @@
-import UncertainData: 
-    UncertainDataset, 
-    resample, 
-    UncertainValue, 
-    StrictlyIncreasing,
-    
-    NoConstraint,
-    TruncateLowerQuantile,
-    TruncateUpperQuantile,
-    TruncateQuantiles,
-    TruncateMinimum,
-    TruncateMaximum,
-    TruncateRange,
-    TruncateStd
+using Test, UncertainData
+using StatsBase
 
-import Distributions: 
-    Uniform, 
-    Normal
+@testset "StrictlyIncreasing" begin 
+        
+    # Create some uncertain data with decreasing magnitude and zero overlap between values, 
+    # so we're guaranteed that a strictly decreasing sequence through the dataset exists.
+    N = 10
+    t = [ i <= N/2 ? CertainValue(float(i)) : UncertainValue(Normal, i, 1) for i = 1:N]
+    iv = UncertainIndexValueDataset(t, t)
 
-import Test 
+    test_cs = [
+        NoConstraint(), 
+        TruncateLowerQuantile(0.2), 
+        TruncateUpperQuantile(0.2),
+        TruncateQuantiles(0.2, 0.8),
+        TruncateMaximum(50),
+        TruncateMinimum(-50),
+        TruncateRange(-50, 50),
+        TruncateStd(1)
+    ]
+    test_seqs = [
+        StrictlyIncreasing(StartToEnd())
+    ]
 
-# Create some uncertain data with increasing magnitude and zero overlap between values, 
-# so we're guaranteed that a strictly increasing sequence through the dataset exists.
-N = 10
-u_timeindices = [ i <= N/2 ? CertainValue(float(i)) : UncertainValue(Normal, i, 1) for i = 1:N]
-UI = UncertainIndexDataset(u_timeindices)
-UV = UncertainValueDataset(u_timeindices)
+    @testset "$(test_seqs[i])" for i in 1:length(test_seqs)
+        @test resample(t, StrictlyIncreasing()) isa Vector{Float64}
 
-# No further constraints other than the order constraint
-x = resample(UI, StrictlyIncreasing())
-@test x isa Vector{Float64}
+        iv_draw = resample(iv, StrictlyIncreasing())
+        @test iv_draw isa Tuple{Vector{Float64}, Vector{Float64}}
 
-n_realizations = 100
-X = [resample(UI, StrictlyIncreasing()) for i = 1:n_realizations]
+        @testset "$(test_cs[i])" for i in 1:length(test_cs)
+            c = test_cs[i] # sequential + single constraint
+            cs = [sample(test_cs, 1) for k = 1:N] # sequential + multiple constraints
+            @test resample(t, StrictlyIncreasing(), c) isa Vector{Float64}
+            @test resample(t, StrictlyIncreasing(), cs) isa Vector{Float64}
 
-# We're getting vectors 
-@test all([x isa Vector{Float64} for x in X])
+            # Single extra constraint
+            iv_draw = resample(iv, StrictlyIncreasing(), c)
+            @test iv_draw isa Tuple{Vector{Float64}, Vector{Float64}}
+            @test all(diff(iv_draw[1]) .> 0)
 
-# All sequences 
-@test all([all(diff(x) .> 0) for x in X])
+            iv_draw = resample(iv, StrictlyIncreasing(), c, c)
+            @test iv_draw isa Tuple{Vector{Float64}, Vector{Float64}}
+            @test all(diff(iv_draw[1]) .> 0)
 
-test_constraints = [
-    NoConstraint(), 
-    TruncateLowerQuantile(0.2), 
-    TruncateUpperQuantile(0.2),
-    TruncateQuantiles(0.2, 0.8),
-    TruncateMaximum(50),
-    TruncateMinimum(-50),
-    TruncateRange(-50, 50),
-    TruncateStd(1)
-]
+            # Multiple extra constraints
+            iv_draw = resample(iv, StrictlyIncreasing(), cs)
+            @test iv_draw isa Tuple{Vector{Float64}, Vector{Float64}}
+            @test all(diff(iv_draw[1]) .> 0)
 
+            iv_draw = resample(iv, StrictlyIncreasing(), cs, cs)
+            @test iv_draw isa Tuple{Vector{Float64}, Vector{Float64}}
+            @test all(diff(iv_draw[1]) .> 0)
 
-# First constrain using a single regular constraint, then apply the order constraint.  
-for i = 1:length(test_constraints)
-    constraint = test_constraints[i]    
-    @test resample(UI, constraint, StrictlyIncreasing()) isa Vector{Float64}
-    @test all([resample(UI, constraint, StrictlyIncreasing()) isa Vector{Float64} for k = 1:n_realizations])
+            iv_draw = resample(iv, StrictlyIncreasing(), c, cs)
+            @test iv_draw isa Tuple{Vector{Float64}, Vector{Float64}}
+            @test all(diff(iv_draw[1]) .> 0)
+
+            iv_draw = resample(iv, StrictlyIncreasing(), cs, c)
+            @test iv_draw isa Tuple{Vector{Float64}, Vector{Float64}}
+            @test all(diff(iv_draw[1]) .> 0)
+        end
+    end
 end
-
-#First element-wise apply a vector of regular constraints to each element in the dataset, 
-#then apply the order constraint.  
-for i = 1:length(test_constraints)
-    constraints = [test_constraints[i] for k = 1:length(UI)]
-    @test resample(UI, constraints, StrictlyIncreasing()) isa Vector{Float64}
-    @test all([resample(UI, constraints, StrictlyIncreasing()) isa Vector{Float64} for k = 1:n_realizations])
-end
-
-
-# Index-value 
-iv = UncertainIndexValueDataset(UI, UV)
-
-# Sequential
-@test resample(iv, StrictlyIncreasing()) isa Tuple{Vector{Float64}, Vector{Float64}}
-
-# First constrain using a single regular constraint, then apply the order constraint.  
-for i = 1:length(test_constraints)
-    constraint = test_constraints[i]
-    @test resample(iv, constraint, StrictlyIncreasing()) isa Tuple{Vector{Float64}, Vector{Float64}}
-    @test resample(iv, constraint, constraint, StrictlyIncreasing()) isa Tuple{Vector{Float64}, Vector{Float64}}
-    @test all([resample(iv, constraint, StrictlyIncreasing()) isa Tuple{Vector{Float64}, Vector{Float64}} for k = 1:n_realizations])
-    @test all([resample(iv, constraint, constraint, StrictlyIncreasing()) isa Tuple{Vector{Float64}, Vector{Float64}} for k = 1:n_realizations])
-
-    cs = [test_constraints[i] for k = 1:length(UI)]
-
-    @test resample(iv, cs, cs, StrictlyIncreasing()) isa Tuple{Vector{Float64}, Vector{Float64}}
-    @test resample(iv, cs, constraint, StrictlyIncreasing()) isa Tuple{Vector{Float64}, Vector{Float64}}
-    @test resample(iv, constraint, cs, StrictlyIncreasing()) isa Tuple{Vector{Float64}, Vector{Float64}}
-end
-
